fa321e926b77bdc0dfd726461a04618efe7fb7ff
Stojanov97 <stojanov091@gmail.com>
2023-07-01 02:00:36 +0200

    update

--- Domasni/8/scripts/1.js
+++ Domasni/8/scripts/1.js
@@ -4,6 +4,62 @@ function product(name, category, hasDiscount, price) {
   this.hasDiscount = hasDiscount;
   this.price = price;
 }
+let hasDiscount = (arrayMain, discount, noDiscount) => {
+  arrayMain
+    .filter((product) => product.hasDiscount == true)
+    .forEach((product) => discount.push(product));
+
+  arrayMain
+    .filter((product) => product.hasDiscount == false)
+    .forEach((product) => noDiscount.push(product));
+};
+
+let greaterThan20 = (array) => {
+  let greaterThan20 = [];
+  array
+    .filter((product) => product.price > 20)
+    .forEach((product) => greaterThan20.push(product.name));
+  return `-Products that cost over 20-\n${greaterThan20.join(", ")}`;
+};
+
+let searchCategory = (array, category) => {
+  let searchedCategory = [];
+  array
+    .filter((product) => product.category == category)
+    .forEach((product) => searchedCategory.push(product.name));
+  if (array == productsOnDiscount) {
+    return `-Products that are in ${category} and are on discount-\n${searchedCategory.join(
+      ", "
+    )}`;
+  } else if (array == productsNoDiscount) {
+    return `-Products that are in ${category} and are not on discount-\n${searchedCategory.join(
+      ", "
+    )}`;
+  } else {
+    return `-Products that are in ${category} category-\n${searchedCategory.join(
+      ", "
+    )}`;
+  }
+};
+
+let avg = (array) => {
+  let prices = [];
+  array.forEach((product) => prices.push(product.price));
+  if (array == productsOnDiscount) {
+    return `-Average price on products that are not on discount-\n${Math.round(
+      prices.reduce((acc, curr) => acc + curr, 0) / array.length
+    )}`;
+  } else if (array == productsNoDiscount) {
+    return `-Average price on products that are not on discount-\n${Math.round(
+      prices.reduce((acc, curr) => acc + curr, 0) / array.length
+    )}`;
+  } else {
+    return `-Average price of the products-\n${Math.round(
+      prices.reduce((acc, curr) => acc + curr, 0) / array.length
+    )}`;
+  }
+};
+
 let products = [
   new product("Turbo", "CarParts", false, 30000),
   new product("Wheel", "CarParts", false, 15000),
@@ -21,3 +77,10 @@ let products = [
   new product("Soap", "Chemical", true, 120),
   new product("Antiperspirant", "Chemical", true, 150),
 ];
+
+let productsOnDiscount = [];
+let productsNoDiscount = [];
+hasDiscount(products, productsOnDiscount, productsNoDiscount);
+console.log(greaterThan20(products));
+console.log(searchCategory(productsOnDiscount, "Food"));
+console.log(avg(productsOnDiscount));
